71d482daadf2f2e8cc51653e8a26cdd80538f672
23786 <yixuanzhuzhuzhu123@163.com>
2020-08-27 13:24:02 +0800

    Add some buttons in console view

--- C+++/Classes/Debug/CDConsoleView.swift
+++ C+++/Classes/Debug/CDConsoleView.swift
@@ -13,9 +13,40 @@ class CDConsoleView: NSView, CDConsoleTextViewDelegate {
     @IBOutlet weak var titleLabel: NSTextField!
     @IBOutlet weak var textView: CDConsoleTextView!
     
+    private func sendInput(_ string: String) {
+        (self.window?.windowController?.document as! CDCodeDocument).sendInputToDebugger(message: string)
+    }
+    
     func consoleTextViewWillInsertNewLine(_ consoleTextView: CDConsoleTextView, lastLineText: String) {
-        (self.window?.windowController?.document as! CDCodeDocument).sendInputToDebugger(message: lastLineText)
-        
+        self.sendInput(lastLineText)
+    }
+    
+    @IBAction func run(_ sender: Any?) {
+        self.sendInput("run")
+    }
+    
+    @IBAction func next(_ sender: Any?) {
+        self.sendInput("next")
+    }
+    
+    @IBAction func `continue`(_ sender: Any?) {
+        self.sendInput("continue")
+    }
+    
+    @IBAction func stepIn(_ sender: Any?) {
+        self.sendInput("step")
+    }
+    
+    @IBAction func stepOut(_ sender: Any?) {
+        self.sendInput("finish")
+    }
+    
+    @IBAction func quit(_ sender: Any?) {
+        self.sendInput("quit")
+    }
+    
+    @IBAction func frameVariable(_ sender: Any?) {
+        self.sendInput("frame variable")
     }
     
 }
