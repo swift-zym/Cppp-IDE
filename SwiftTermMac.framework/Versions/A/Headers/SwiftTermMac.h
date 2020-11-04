e563957e3b24d9f0203ddd89ecdf024526efac64
23786 <yixuanzhuzhuzhu123@163.com>
2020-11-04 18:13:06 +0800

    Add SwiftTermMac framework

---
+++ SwiftTermMac.framework/Versions/A/Headers/SwiftTermMac.h
@@ -0,0 +1,18 @@
+//
+//  SwiftTermMac.h
+//  SwiftTermMac
+//
+//  Created by 23786 on 2020/11/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftTermMac.
+FOUNDATION_EXPORT double SwiftTermMacVersionNumber;
+
+//! Project version string for SwiftTermMac.
+FOUNDATION_EXPORT const unsigned char SwiftTermMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTermMac/PublicHeader.h>
+
+
