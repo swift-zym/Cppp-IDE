a4636efba0221fa5ab23058ebe2a7d4dff18441d
23786 <yixuanzhuzhuzhu123@163.com>
2020-11-20 23:11:46 +0800

    Update Settings View Controller

--- C+++/Classes/Settings/CDEditorPreferencesViewController.swift
+++ C+++/Classes/Settings/CDEditorPreferencesViewController.swift
@@ -13,6 +13,26 @@ class CDEditorPreferencesViewController: NSViewController {
     @IBOutlet weak var showsCompletionList: NSButton!
     @IBOutlet weak var autoComplete: NSButton!
     @IBOutlet weak var displaysTooltip: NSButton!
+    @IBOutlet weak var fontNameLabel: NSTextField!
+    
+    @IBAction func chooseAnotherFont(_ sender: Any?) {
+        
+        NSFontPanel.shared.setPanelFont(CDSettings.font, isMultiple: false)
+        NSFontManager.shared.target = self
+        NSFontManager.shared.action = #selector(changeFont(_:))
+        NSFontManager.shared.orderFrontFontPanel(self)
+        
+    }
+    
+    @objc func changeFont(_ sender: Any?) {
+        
+        let font = CDSettings.font
+        let convertedFont = NSFontPanel.shared.convert(font)
+        self.fontNameLabel.stringValue = "Font: " + convertedFont.fontName + " Size: \(Int(convertedFont.pointSize))"
+        CDSettings.fontSize = Int(convertedFont.pointSize)
+        CDSettings.fontName = convertedFont.fontName
+        
+    }
     
     @IBAction func showsCompletionListButtonClicked(_ sender: NSButton) {
         CDSettings.codeCompletion = sender.state == .on
@@ -32,6 +52,7 @@ class CDEditorPreferencesViewController: NSViewController {
         self.showsCompletionList.setState(CDSettings.codeCompletion)
         self.autoComplete.setState(CDSettings.autoComplete)
         self.displaysTooltip.setState(CDSettings.displaysTooltipOfCode)
+        self.fontNameLabel.stringValue = "Font: " + CDSettings.fontName + " Size: \(Int(CDSettings.fontSize))"
         
     }
     
