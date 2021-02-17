cdf48de5ac5eb1d6c9d2a06dc31d71ef4aa4410f
Yiming Zhang <i@zhangyiming.tech>
2021-02-17 20:14:58 +0800

    add competitive companion support

--- C+++/Classes/Server/CDServer.swift
+++ C+++/Classes/Server/CDServer.swift
@@ -19,7 +19,33 @@ class CDServer : NSObject{
     func start(){
         let ccRoute=Route(method: .post, uri: "/", handler: {
             req,res in
-            print(req.postBodyString ?? "NULL")
+            if req.postBodyString == nil{
+                return
+            }
+            let json = try! JSONSerialization.jsonObject(with: (req.postBodyString?.data(using: .utf8))!, options: .mutableContainers) as AnyObject
+            let tests = json["tests"] as! [AnyObject]
+            var inputs=[String](),outputs=[String]()
+            for test in tests{
+                inputs.append(test["input"] as! String)
+                outputs.append(test["output"] as! String)
+            }
+            DispatchQueue.main.async {
+                let newDocument = CDCodeDocument()
+                newDocument.content=CDDocumentContent(contentString: """
+                //
+                // problem name:\(json["name"] as! String)
+                // problem link:\(json["url"] as! String)
+                // memory limit:\(json["memoryLimit"] as! Int)
+                // time limit:\(json["timeLimit"] as! Int)
+                //
+                """)
+                
+                GlobalMainWindowController.addDocument(newDocument)
+                if inputs.count != 0{
+                    GlobalMainWindowController.mainViewController.consoleView.runView.input?.text=inputs[0]
+                    GlobalMainWindowController.mainViewController.consoleView.runView.expectedOutput?.text=outputs[0]
+                }
+            }
         })
         let serialQueue = DispatchQueue.init(label: "", qos: .default, attributes: [.concurrent], autoreleaseFrequency: .inherit, target: nil)
         serialQueue.async {
